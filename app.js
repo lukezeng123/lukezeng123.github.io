46fd49436215b7a14282b2e489c176f0b31bde49
Luke Zeng <z5479537@ad.unsw.edu.au>
2024-12-11 19:47:07 +1100

    created basic stuff

--- app.js
+++ app.js
@@ -4,9 +4,30 @@ document.addEventListener('DOMContentLoaded', function() {
   const button = document.getElementById('change-text-btn');
   const mainHeading = document.getElementById('main-heading');
   const form = document.getElementById('contact-form');
+  const tabLinks = document.querySelectorAll('.tab-link');
+  const tabContents = document.querySelectorAll('.tab-content');
 
+  tabLinks.forEach(tab => {
+    tab.addEventListener('click', () => {
+      tabLinks.forEach(link => link.classList.remove('active'));
+      tabContents.forEach(content => content.classList.remove('active'));
+
+      tab.classList.add('active');
+      document.getElementById(tab.dataset.tab).classList.add('active');
+
+      if (tab.dataset.tab === 'secret') {
+        const password = prompt('Enter the password to reveal the secret:');
+        if (password ==='meowfriend123') {
+          targetTab.classList.add('active');
+          document.getElementById('secret').style.display = 'block';
+        } else {
+          alert('Incorrect Password!');
+        }
+      }
+    })
+  })
   button.addEventListener('click', function() {
-      mainHeading.textContent = 'Welcome to T1S Tuition!';
+      mainHeading.textContent = 'Welcome to Luke\'s Website!';
   });
 
   form.addEventListener('submit', function(event) {
